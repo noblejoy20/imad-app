a03da7b1084c7ebcbd89e4cacb512a778aa0907c
Noble Joy <noblejoy20@gmail.com>
2017-08-14 22:10:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/ui/locator.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'locator.html'));
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });

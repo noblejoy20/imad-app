66754a7ce9a950c106ca1bb5f6bba92bd6aa9bb0
Noble Joy <noblejoy20@gmail.com>
2017-09-05 00:04:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ app.get('/article-one',function(req,res){
     res.send('not found');
 })
 
+var counter=0;
+app.get('/counter',function(req,res){
+    counter=counter+1;
+    res.send(counter.toString());
+})
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });

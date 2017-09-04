65cdc39ff0206259db55d5444631f8cc6b670465
Noble Joy <noblejoy20@gmail.com>
2017-09-05 01:20:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,17 @@
-console.log('Loaded!');
+var button= document.getElementById('counter');
+button.onclick=function(){
+    var request=new XMLHttpRequest();
+    request.onreadystatechange=function(){
+        if(this.readystate==XMLHttpRequest.DONE)
+        {
+            if(this.status==200)
+            {
+              var counter=request.responseText();
+              var span=document.getElementById('count');
+              span.innerHTML=counter.toString();
+            }
+        }
+    };
+    request.open('GET','http://noblejoy20.imad.hasura-app.io/counter',true);
+    request.send(null);
+};
